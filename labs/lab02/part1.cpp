88104ea77614feb05b1e3c4394fe61a9453006d1
kimh2016 <89935597+kimh2016@users.noreply.github.com>
2022-02-02 16:00:44 -0500

    Add files via upload

---
+++ labs/lab02/part1.cpp
@@ -0,0 +1,30 @@
+/* part1.cpp
+ * Name: Hansol Kim
+ * Date:  September 14, 2021
+ * Begun by: Joel Adams, for CS 112 at Calvin University.
+ */
+
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	long double   ldoubleVal; cout << "ldoubleVal: " << &ldoubleVal << endl;
+//	double        doubleVal; cout << "doubleVal: " << &doubleVal << endl;
+//	float         floatVal; cout << "floatVal: " << &floatVal << endl;
+//	long          longVal; cout << "longVal: " << &longVal << endl;
+//	int           intVal; cout << "intVal: " << &intVal << endl;
+//	short         shortVal; cout << "shortVal: " << &shortVal << endl;
+//	char          charVal1; cout << "charVal1: " << static_cast<void*>(&charVal1) << endl;
+//	char          charVal2; cout << "charVal2: " << static_cast<void*>(&charVal2) << endl;
+//	bool          boolVal; cout << "boolVal: " << &boolVal << endl;
+//
+//	long double * ldPtr; cout << "*ldPtr: " << &ldPtr << endl;
+//	double *      dPtr; cout << "*dPtr: " << &dPtr << endl;
+//	float *       fPtr; cout << "*fPtr: " << &fPtr << endl;
+//	long *        lPtr; cout << "*lPtr: " << &lPtr << endl;
+//	int *         iPtr; cout << "*iPtr: " << &iPtr << endl;
+//	short *       sPtr; cout << "*sPtr: " << &sPtr << endl;
+//	char *        cPtr; cout << "*cPtr: " << &cPtr << endl;
+//	bool *        bPtr; cout << "*bPtr: " << &bPtr << endl;
+//}
+
