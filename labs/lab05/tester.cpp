684fb6ca5e887427fa8862455d3ccf7e3be8e267
kimh2016 <89935597+kimh2016@users.noreply.github.com>
2022-02-02 16:04:45 -0500

    Create tester.cpp

---
+++ labs/lab05/tester.cpp
@@ -0,0 +1,15 @@
+/* tester.cpp drives the testing of our Vec template and Matrix class.
+ * Student Name: Hansol Kim (hk94), Oghenesuvwe Ogedegbe (oko2)
+ * Date: 28 September 2021
+ * Begun by: Joel C. Adams, for CS 112 at Calvin University.
+ */
+
+#include "VecTester.h"
+#include "MatrixTester.h"
+
+int main() {
+//	VecTester vt;
+//	vt.runTests();
+	MatrixTester mt;
+	mt.runTests();
+}
