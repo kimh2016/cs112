1dceace478be5eae9d7d822bbdde1120aeaaac57
kimh2016 <89935597+kimh2016@users.noreply.github.com>
2022-02-02 15:53:51 -0500

    Create main.cpp

---
+++ labs/lab01/main.cpp
@@ -0,0 +1,19 @@
+/*
+ * main.cpp
+ * Student Name and UserId: Hansol Kim hk94
+ * Date: Sep 4, 2021
+ * Begun by: Hansol Kim, for CS 112 lab at Calvin University.
+ */
+
+#include "SongTester.h"
+#include "PlayListTester.h"
+#include <iostream>
+
+int main() {
+	SongTester sTester;
+	sTester.runTests();
+	PlayListTester plTester;
+	plTester.runTests();
+
+
+}
