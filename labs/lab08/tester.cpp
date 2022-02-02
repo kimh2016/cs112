d64b85b51314d15fbb376c4b66283025c4306451
kimh2016 <89935597+kimh2016@users.noreply.github.com>
2022-02-02 16:07:27 -0500

    Create tester.cpp

---
+++ labs/lab08/tester.cpp
@@ -0,0 +1,13 @@
+/* tester.cpp drives the testing of the Queue classes.
+ * Joel Adams, for CS 112 at Calvin University.
+ */
+
+ #include "ArrayQueueTester.h"
+ #include "LinkedQueueTester.h"
+ 
+ int main() {
+ 	ArrayQueueTester aqt;
+ 	aqt.runTests();
+ 	LinkedQueueTester lqt;
+ 	lqt.runTests();
+ }
