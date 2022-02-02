8a4c1fcef526dc09c0cf3a01be026ae276436dfb
kimh2016 <89935597+kimh2016@users.noreply.github.com>
2022-02-02 16:19:54 -0500

    Create main.cpp

---
+++ projects/proj02/main.cpp
@@ -0,0 +1,13 @@
+/* main.cpp drives the testing of the array operations.
+ *
+ * Student: Hansol Kim (hk94)
+ * Date: September 14, 2021
+ * Begun by: Joel Adams, for CS 112 at Calvin University.
+ */
+ 
+#include "ArrayTester.h"
+
+int main() {
+	ArrayTester at;
+	at.runTests();
+}
